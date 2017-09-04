16bf6e67a91d76f688d201c70adc24df1a36a5a7
Marco Antonio Condori Challco <m.condori.ch92@gmail.com>
2017-09-04 12:29:42 +0200

    Mi Primer commit realizado

---
+++ curriculum.php
@@ -0,0 +1,157 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="theme-color" content="#022049" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimum-scale=1.0">
+	<title>DISEÑO PERSONAL</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/estilo.css">
+	<link rel="stylesheet" href="fonts.css"> 
+	<link rel="shortcut icon" href="img/indice.ico" type="image/x-icon">
+	</head>
+
+<body>
+	<div id="contenedor">
+		<!-- Cabecera -->
+		<header>
+			<h1>Emergentes 1</h1>
+			<img src="img/indice.ico">
+			<!--<embed src="img/indice.ico" type="image/svg+xml" /> -->
+		</header>
+		<!--<nav>
+			<ul>
+				<li><a href="index.html"><span class="icon-home"></span>Inicio</a></li>
+				<li class="activo"><a href="quien.html"><span class="icon-man"></span>Quien soy</a></li>
+				<li><a href="blog.html"><span class="icon-news"></span>Blog</a></li>
+				<li><a href="contacto.html"><span class="icon-old-phone"></span>Contactame</a></li>
+			</ul>
+		</nav>-->
+		<!-- Contenido -->
+		<div class="cuerp">
+			<section class="quien">
+				<article>
+					<h1 class="titu" id="vitae">CURRICULUM VITAE</h1>
+					<div> <img src="img/colores-crop-u3299.png" alt="" width="700px">
+						<h2 id="datos">Datos Personales</h2>
+						<table>
+							<tr>
+								<td><strong>Celular:</strong></td>
+								<td></td>
+								<td><?php echo "77578934"." con php"; ?></td>
+							</tr>
+							<tr>
+								<td><strong>E-mail:</strong></td>
+								<td></td>
+								<td>m.condori.ch92@gmail.com</td>
+							</tr>
+							<tr>
+								<td><strong>Nacimiento:</strong></td>
+								<td></td>
+								<td>24-01-1992 en La Paz Bolivia</td>
+							</tr>
+							<tr>
+								<td><strong>Estado Civil:</strong></td>
+								<td></td>
+								<td>Soltero</td>
+							</tr>
+							<tr>
+								<td><strong>Carnet de Identidad: </strong></td>
+								<td></td>
+								<td>8387153 LP</td>
+							</tr>
+						</table> <img src="img/colores-crop-u3299.png" alt="" width="700px">
+						<h2 id="formacion">Formación Académica</h2>
+						<table>
+							<tr>
+								<td><strong>Estudios Primarios:</strong></td>
+								<td>unidad educativa tokio</td>
+							</tr>
+							<tr>
+								<td><strong>Estudios Secundarios:</strong></td>
+								<td>Colegio Nacional Mixto Nacional Puerto de Guaqui</td>
+							</tr>
+							<tr>
+								<td><strong>Estudios Universitarios:  .</strong></td>
+								<td>Universidad Publica de El Alto</td>
+							</tr>
+						</table> <img src="img/colores-crop-u3299.png" alt="" width="700px">
+						<h2>Conocimientos en Informática</h2>
+						<table>
+							<tr>
+								<td><strong>Programación:</strong></td>
+								<td>Java, c++, c#, Netbeans, Eclipse, Android Studio</td>
+							</tr>
+							<tr>
+								<td><strong>Ofimática:</strong></td>
+								<td>Microsoft Office, OpenOffice</td>
+							</tr>
+							<tr>
+								<td><strong>DBA:</strong></td>
+								<td>Oracle, MySql, PL/SLQ, SQLite,Microsoft Acces</td>
+							</tr>
+						</table> <img src="img/colores-crop-u3299.png" alt="" width="700px">
+						<h2 id="idiomas">Idiomas</h2>
+						<table>
+							<tr>
+								<td><strong>Español:</strong></td>
+								<td>Nivel Alto</td>
+							</tr>
+							<tr>
+								<td><strong>Inglés:</strong></td>
+								<td>Nivel Medio</td>
+							</tr>
+							<tr>
+								<td><strong>Aymará:</strong></td>
+								<td>Nivel Medio</td>
+							</tr>
+						</table> <img src="img/colores-crop-u3299.png" alt="" width="700px">
+						<h2 id="otros">Otros</h2>
+						<table>
+							<tr>
+								<td>Disponibilidad de viajar</td>
+							</tr>
+						</table> <img src="img/colores-crop-u3299.png" alt="" width="700px">
+						
+					</div>
+				</article>
+			</section>
+			<!-- Contenido relacionado-->
+			<aside>
+				<!-- Sobre mi -->
+				<h3>Sobre mí</h3>
+				<figure> <img src="img/antonio.jpg" alt="Mi foto" width="100px"> </figure>
+				<p><strong>Marco Antonio Condori Challco</strong>
+					<br /> El Alto - La Paz
+					<br /> <a href="#">Mi perfil público</a></p>
+				<h3>Materias</h3>
+				<ul>
+					<li><a href="#">Emergentes I</a></li>
+					<li><a href="#">Auditoria en Sistemas</a></li>
+					<li><a href="#">Redes II</a></li>
+					<li><a href="#">Ingenieria Economica</a></li>
+					<li><a href="#">Ingenieria de sistemas II</a></li>
+				</ul>
+				<h3>Archivos</h3>
+				<ul>
+					<li><a href="#">I/2014 (1er Semestre)</a></li>
+					<li><a href="#">II/2014 (2do Semestre)</a></li>
+					<li><a href="#">I/2015 (3er Semestre)</a></li>
+					<li><a href="#">II/2015 (4to Semestre)</a></li>
+					<li><a href="#">I/2016 (5to Semestre)</a></li>
+					<li><a href="#">II/2016 (6to Semestre)</a></li>
+					<li><a href="#">I/2017 (7mo Semestre)</a></li>
+					<li><a href="#">II/2017 (8vo Semestre)</a></li>
+				</ul>
+			</aside>
+			<!-- Pie de pagina -->
+			<footer>
+				<p>&copy 2017 Emergentes </p> >
+		    </footer>
+		</div>
+	</div>
+</body>
+
+</html>
